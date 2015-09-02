b2dfd449dffef2ba5378d8cf34cb05f5014a7b35
Thomas Tien <ttien@hearst.com>
2015-09-02 18:42:23 -0400

    Python solution for https://leetcode.com/problems/valid-anagram/
    	new file:   anagram.py
    	modified:   test.py

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 
-from digit import Solution
+#from digit import Solution
+from anagram import Solution
 
 answer = Solution()
-print answer.addDigits(9999)
-
+print answer.isAnagram('foobar', 'raboof')
+print answer.isAnagram('hello', 'world')
+print answer.isAnagram('hehe', 'heh')
+print answer.isAnagram('heh', 'hehe')
+print answer.isAnagram('bluebells', 'bells')
+print answer.isAnagram('able wasi', 'iwasable')
+print answer.isAnagram('ablewasi', 'iwasable')

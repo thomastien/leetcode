e97dc448de5b92692ec3fd9efb2e1bddbe4b533d
Thomas Tien <ttien@hearst.com>
2015-09-04 21:57:07 -0400

    algo_power2 	new file:   power.py 	new file:   power.test.py
    	https://leetcode.com/problems/power-of-two/?
    	https://leetcode.com/submissions/detail/38813083/

---
+++ power.test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from power import Solution
+
+answer = Solution()
+
+print '25 got + ' + str(answer.isPowerofTwo(25))
+print '64 got + ' + str(answer.isPowerofTwo(64))
+print '1 got + ' + str(answer.isPowerofTwo(1))
+print '0 got + ' + str(answer.isPowerofTwo(0))
+print '2 got + ' + str(answer.isPowerofTwo(2))
+print '5 got + ' + str(answer.isPowerofTwo(5))
+print '6 got + ' + str(answer.isPowerofTwo(6))
+print '7 got + ' + str(answer.isPowerofTwo(7))
+print '16 got + ' + str(answer.isPowerofTwo(16))
+print '1048576 got + ' + str(answer.isPowerofTwo(1048576))

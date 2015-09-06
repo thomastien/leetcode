b67369672c3fb18c938a33689adfa18f44bcc5b7
Thomas Tien <ttien@hearst.com>
2015-09-06 17:08:20 -0400

    No Memory Error, but Timeout still solution
    	modified:   hindex.py
    	modified:   hindex.test.py

--- hindex.py
+++ hindex.py
@@ -6,10 +6,6 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        #pp = pprint.PrettyPrinter(indent=4)
-        #pp.pprint(citations)
-
-        highest_score = 0; 
         scoresDict = {}
 
         # If there are 5 papers, say 2, 2, 3, 4, 5
@@ -23,19 +19,34 @@ class Solution(object):
         # scoresDict[3] + 1
         # etc...then we iterate through all scoresDict high->low and find the highest one where index >= value
 
+        # Try again, first try got MemoryError
+        # 1. sort it low->high
+        # 2. traverse, the 
+
+        counter = len(citations)
+        #for num in reverse(sorted(citations)):
+            #noop   
+
+        #return(len(sorted(citations)))
 
         #for num in range(len(citations), 0, -1):
+
         for num in range(len(citations)):
-            for score in range(citations[num]+1):
-                scorekey = str(score)
+            #print num
+            cap = citations[num]+1  
+            if cap > len(citations):
+                cap = len(citations)
+            for score in range(cap):
+                scorekey = score 
+                if (scorekey > len(citations)):
+                    scorekey = len(citations)
                 try:
                     scoresDict[scorekey] += int(1)
-                except KeyError:
+                except:
                     scoresDict[scorekey] = int(1)
  
                 #scoresDict[scorekey] = scoresDict[scorekey] + int(1)
                 #print 'score ' + str(score) + ' got' + str(scoresDict[scorekey])
-
 
         for key, value in reversed(sorted(scoresDict.items())): 
             #print str(key) + ' has value ' + str(value)

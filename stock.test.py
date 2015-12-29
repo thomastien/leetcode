1ee8df3a1726a0ac1601dbea7148b9d9c214d1e3
Thomas Tien <ttien@hearst.com>
2015-12-28 19:58:13 -0500

    stocks_problem	Solution for https://leetcode.com/submissions/detail/49130326/
    	new file:   stock.py
    	new file:   stock.test.py

---
+++ stock.test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from stock import Solution
+
+answer = Solution()
+
+print answer.maxProfit([1,2,3,4,5])
+print answer.maxProfit([9,0,2,7,1])
+print answer.maxProfit([1,9,3,6,2,7,5,4,0])
+print answer.maxProfit([7,8,3,6,7,4,7,5,1])

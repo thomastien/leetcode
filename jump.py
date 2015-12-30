b7dfc98ccd75dab9f36a0cae16f05bcbc5c2707f
Thomas Tien <ttien@hearst.com>
2015-12-30 17:46:02 -0500

    On branch algo_jump	Valid solution for https://leetcode.com/problems/jump-game/
    	modified:   jump.py
    	modified:   jump.test.py

--- jump.py
+++ jump.py
@@ -6,16 +6,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        print(nums)
+
+        # ok, basically as long as we don't get stuck at a zero, code can proceed
+        permitted = 0
+
         if (len(nums) == 1):
             return True
-
-        curindex = 1
-        while (curindex < len(nums)):
-            curindex += nums[curindex-1]
-            print curindex
-            if curindex > len(nums):
-                return True
-            if nums[curindex-1] == 0:
-                return False
-        return True
+        for i in xrange(1, len(nums)):
+            if nums[i-1] == 0 and i >= permitted:
+                return False 
+            if i+nums[i-1] >= len(nums):
+                return True 
+            # Permitted is used to monitor if we should keep things goin
+            #   this is need for cases like [3,0,8,2,0,0,1]
+            if (i+nums[i-1] > permitted) :
+                permitted = i+nums[i-1]
